13da43823bf1e8698782028b60d0364f4b71f77a
Robert <chiu.robert96@gmail..com>
2017-03-11 10:15:59 +0200

    Latest update

---
+++ Assets/Scripts/StarsController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarsController : MonoBehaviour {
+
+    ParticleSystem.ShapeModule shapeModule;
+    public Rigidbody rb;
+    // Use this for initialization
+    private float distance;
+
+    public GameObject earth;
+	void Start () {
+        distance = transform.position.z - earth.transform.position.z;
+        rb = GetComponent<Rigidbody>();
+        //shapeModule = GetComponent<ParticleSystem>().shape;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //obj = transform.FindChild("Moving Stars");
+        //transform.Translate(v * maxSpeed * Time.deltaTime);//obj.GetComponentInChildren<Transform>().transform);
+        //rb.MovePosition(transform.position +  * Time.deltaTime);
+        //transform.Translate(Vector3.forward * Time.deltaTime, Camera.main.transform);
+        this.transform.position = earth.transform.position + new Vector3(-earth.transform.position.x , -earth.transform.position.y ,distance);
+        //shapeModule.radius = 4000;
+        Debug.Log(earth.transform.position.z + " - " + transform.position.z);
+	}
+}
