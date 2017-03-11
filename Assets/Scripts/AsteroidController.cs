627dad5abc79002e05bd0c4c6164fd2508e47b50
Robert <chiu.robert96@gmail..com>
2017-03-11 14:31:30 +0200

    Started planetary gravity and metor trajectory

---
+++ Assets/Scripts/AsteroidController.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidController : MonoBehaviour {
+
+    Vector3 lastPos;
+    Transform obj;
+
+    public GameObject planet;
+    Rigidbody rb;
+    Vector3 direction;
+
+    float lastCheckTime;
+    Vector3 lastCheckPos;
+    float xSeconds;
+    float zMuch;
+
+    bool changedPos;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        lastCheckTime = 0;
+        xSeconds = 3.0f;
+        zMuch = 1.0f;
+
+        obj = planet.GetComponent<Rigidbody>().transform;
+        lastPos = obj.position;
+
+        changedPos = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        direction = planet.transform.position - this.transform.position;
+        //Debug.Log("----- " + direction);
+
+        if (direction.x >= 3 && direction.y >= 0.3 && (direction.z >= 3 || direction.z <= -3))
+            this.transform.rotation = Quaternion.LookRotation(direction);
+        transform.position += transform.forward * Time.deltaTime * 8;
+        /*
+        Vector3 offset = obj.position - lastPos;
+
+        if(offset.x > 0.1 || offset.y > 0.1 || offset.x < -0.1 || offset.y < -0.1)
+        {
+            lastPos = obj.position;
+            transform.position += transform.forward * Time.deltaTime * 8;
+            changedPos = true;
+        }
+        else
+        {
+            if (changedPos == false)
+            {
+                this.transform.rotation = Quaternion.LookRotation(direction);
+                transform.position += transform.forward * Time.deltaTime * 8;
+            }
+        }
+        */
+        //Log(lastPos + " // " + offset);
+        /*
+
+        transform.position += transform.forward * Time.deltaTime * 8;
+        */
+        /*
+        if ((Time.time - lastCheckTime) > xSeconds)
+        {
+            if ((planet.transform.position - lastCheckPos).magnitude < zMuch)
+            {
+                //Debug.Log("da");
+                transform.position += transform.forward * Time.deltaTime * 8;
+            }
+            else
+            {
+                this.transform.rotation = Quaternion.LookRotation(direction);
+                transform.position += transform.forward * Time.deltaTime * 8;
+            }
+            lastCheckPos = planet.transform.position;
+            lastCheckTime = Time.time;
+        }
+        Debug.Log((planet.transform.position - lastCheckPos).magnitude + " / " + (Time.time - lastCheckTime));
+        */  
+    }
+}
--- Assets/Scripts/Gravity.cs
+++ Assets/Scripts/Gravity.cs
@@ -4,84 +4,91 @@ using UnityEngine;
 
 public class Gravity : MonoBehaviour
 {
-    Vector3 velocity = new Vector3(5, 5, 0);
-    //Vector3 velocity;
-    float gravity = 9;
+    public GameObject Earth;
+    public GameObject planet;
 
-    // Update is called once per frame
-    void FixedUpdate () {
-        velocity.y = velocity.y - gravity * Time.deltaTime;
-
-        transform.position = transform.position + velocity * Time.deltaTime;
-	}
-
-    /* 2nd  public List<GameObject> affected = new List<GameObject>();
-
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.GetComponent<Rigidbody>() != null && other.tag != "Projectile")
-        {
-            affected.Add(other.gameObject);
-        }
-    }
-
-    void OnTriggerExit(Collider other)
-    {
-        if (affected.IndexOf(other.gameObject) != -1)
-        {
-            affected.Remove(other.gameObject);
-        }
-    }
-
+    public float gravitationalPull;
 
     void FixedUpdate()
     {
-        foreach (GameObject cur in affected)
+        //apply spherical gravity to the planet
+        if (Earth.GetComponent<Rigidbody>())
         {
-            float distance = Vector3.Distance(transform.position , cur.transform.position);
-            cur.GetComponent<Rigidbody>().AddForce(Vector3.Normalize(transform.position - cur.transform.position) * (1 / distance * distance) * Gravity);
-            //this is basically newton's gravity law with the two masses each being one
+                if(Earth.GetComponent<Rigidbody>().velocity.z <= Controller.staticForwardSpeed)
+            Earth.GetComponent<Rigidbody>().AddForce((planet.transform.position - Earth.transform.position).normalized * gravitationalPull);
+            Debug.Log((planet.transform.position - Earth.transform.position).normalized * gravitationalPull);
+            Debug.Log(Earth.GetComponent<Rigidbody>().velocity.z + " ** " + Controller.staticForwardSpeed);
         }
-    }*/
-
-    /*3rd public float pullRadius = 100;
-    public float pullForce = 20;
-
-    public void FixedUpdate()
-    {
-        foreach (Collider collider in Physics.OverlapSphere(transform.position, pullRadius)) 
-        {
-            // calculate direction from target to me
-            Vector3 forceDirection = transform.position - collider.transform.position;
-
-            // apply force on target towards me
-            collider.attachedRigidbody.AddForce(forceDirection.normalized * pullForce * Time.fixedDeltaTime);
-        }
-    }*/
-    /*[SerializeField]
-    private GameObject meteor;
-
-    [SerializeField]
-    private Camera Cam;
-
-    [SerializeField]
-    private float minSize, maxSize;
-
-    void Spawn()
-    {
-        GameObject x=Instantiate(meteor, new Vector3(Cam.ScreenToWorldPoint(Input.mousePosition).x, Cam.ScreenToWorldPoint(Input.mousePosition).y, 0), Quaternion.identity) as GameObject;
-
-        float i = Random.Range(minSize, maxSize);
-
-        x.transform.localScale = new Vector3(i, i, i);
     }
 
-    private void Update()
-    {
-        if(Input.GetMouseButtonDown(0))
+
+        /* 2nd  public List<GameObject> affected = new List<GameObject>();
+
+        void OnTriggerEnter(Collider other)
         {
-            Spawn();
+            if (other.GetComponent<Rigidbody>() != null && other.tag != "Projectile")
+            {
+                affected.Add(other.gameObject);
+            }
         }
-    }*/ 
-}
+
+        void OnTriggerExit(Collider other)
+        {
+            if (affected.IndexOf(other.gameObject) != -1)
+            {
+                affected.Remove(other.gameObject);
+            }
+        }
+
+
+        void FixedUpdate()
+        {
+            foreach (GameObject cur in affected)
+            {
+                float distance = Vector3.Distance(transform.position , cur.transform.position);
+                cur.GetComponent<Rigidbody>().AddForce(Vector3.Normalize(transform.position - cur.transform.position) * (1 / distance * distance) * Gravity);
+                //this is basically newton's gravity law with the two masses each being one
+            }
+        }*/
+
+        /*3rd public float pullRadius = 100;
+        public float pullForce = 20;
+
+        public void FixedUpdate()
+        {
+            foreach (Collider collider in Physics.OverlapSphere(transform.position, pullRadius)) 
+            {
+                // calculate direction from target to me
+                Vector3 forceDirection = transform.position - collider.transform.position;
+
+                // apply force on target towards me
+                collider.attachedRigidbody.AddForce(forceDirection.normalized * pullForce * Time.fixedDeltaTime);
+            }
+        }*/
+        /*[SerializeField]
+        private GameObject meteor;
+
+        [SerializeField]
+        private Camera Cam;
+
+        [SerializeField]
+        private float minSize, maxSize;
+
+        void Spawn()
+        {
+            GameObject x=Instantiate(meteor, new Vector3(Cam.ScreenToWorldPoint(Input.mousePosition).x, Cam.ScreenToWorldPoint(Input.mousePosition).y, 0), Quaternion.identity) as GameObject;
+
+            float i = Random.Range(minSize, maxSize);
+
+            x.transform.localScale = new Vector3(i, i, i);
+        }
+
+        private void Update()
+        {
+            if(Input.GetMouseButtonDown(0))
+            {
+                Spawn();
+            }
+        }*/
+    }
 
